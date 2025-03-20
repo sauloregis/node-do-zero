e401bb54363e06dbc43e2c3cb04fe0767315f548
Saulo Regis de Medeiros Costa <l4910@liga.org.br>
2025-03-20 12:40:38 -0300

    a

--- server.js
+++ server.js
@@ -1,64 +1,58 @@
-import { fastify } from 'fastify'
-import { DatabasePostgres } from './database-postgres.js'
+import { fastify } from 'fastify';
+import { DatabasePostgres } from './database-postgres.js';
 import fastifyCors from 'fastify-cors';
 
-
-const server = fastify()
-const database = new DatabasePostgres()
+const server = fastify();
+const database = new DatabasePostgres();
 
 // Habilita o CORS para todas as rotas
 server.register(fastifyCors, {
-    origin: '*', // Permite todas as origens. Ajuste conforme necessário.
-  });
+  origin: '*', // Permite todas as origens. Ajuste conforme necessário.
+});
 
 server.get('/videos', async (request) => {
-    const search = request.query.search
+  const search = request.query.search;
 
-    const videos = await database.list(search)
+  const videos = await database.list(search);
 
-    return videos
-})
+  return videos;
+});
 
 server.post('/videos', async (request, reply) => {
-    const { title, description, duration }= request.body
+  const { title, description, duration } = request.body;
 
-     await database.create({
-        title,
-        description,
-        duration,
-    })
+  await database.create({
+    title,
+    description,
+    duration,
+  });
 
-    return reply.status(201).send()
-})
+  return reply.status(201).send();
+});
 
 server.put('/videos/:id', async (request, reply) => {
-    const videoId = request.params.id
-    const { title, description, duration }= request.body
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
 
-    await database.update(videoId, {
-        title,
-        description,
-        duration,
-    })
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
 
-    return reply.status(204).send()
-})
+  return reply.status(204).send();
+});
 
 server.delete('/videos/:id', async (request, reply) => {
-    const videoId = request.params.id
+  const videoId = request.params.id;
 
-    await database.delete(videoId)
+  await database.delete(videoId);
 
-    return reply.status(204).send()
-})
-
+  return reply.status(204).send();
+});
 
 const port = process.env.PORT || 3333;
 
 server.listen({ port, host: '0.0.0.0' }, () => {
-    console.log(`🚀 Servidor rodando na porta ${port}`);
+  console.log(`🚀 Servidor rodando na porta ${port}`);
 });
-
-// server.listen({
-//     port: process.env.PORT ?? 3333,
-// })
